b3dd820ffca5d8d9bbd927cbc0819d490b36a726
adimosh <adimosh@live.com>
2015-07-07 18:59:04 +0300

    #2 - Added unit tests project.

---
+++ test/IX.Retry.UnitTests/RetryExtensionsUnitTests.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace IX.Retry.UnitTests
+{
+    [TestClass]
+    public class RetryExtensionsUnitTests
+    {
+        int maxRetries;
+        int currentRetry;
+        bool hasRetriedCorrectly;
+
+        void RetryTestMethod()
+        {
+
+        }
+
+        [TestInitialize]
+        public void TestInitialize()
+        {
+            hasRetriedCorrectly = false;
+        }
+
+        [TestMethod]
+        public void RetryExtensions_DoRetryAsync_SimpleAction_OneRetry()
+        {
+        }
+    }
+}
