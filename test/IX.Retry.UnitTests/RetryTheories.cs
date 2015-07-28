24cbd67e7186753c01d2a0bc73a2cf0097738b52
adimosh <adimosh@live.com>
2015-07-28 11:41:32 +0300

    #3 - Created simple test parameter invocation list.

--- test/IX.Retry.UnitTests/RetryTheories.cs
+++ test/IX.Retry.UnitTests/RetryTheories.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Reflection;
 using System.Text;
+using System.Threading;
 using System.Threading.Tasks;
 using Xunit;
 
@@ -14,10 +15,15 @@ namespace IX.Retry.UnitTests
         [MemberData(nameof(RetryTheoryDataGenerator))]
         public void SimpleRetryTheory(int maxRetries, object[] parameters, Type[] exceptionTypes)
         {
+            // ARRANGE
+            // =======
+            
+            // Initialize wrapper
             RetryTestMethodWrapper wrapper = new RetryTestMethodWrapper(maxRetries);
 
             MethodInfo mi;
 
+            // Get proper "With.Retry" method
             MethodInfo[] mis = typeof(With).GetMethods(BindingFlags.Static | BindingFlags.Public);
 
             Type[] parameterTypes = parameters.Aggregate(new List<Type>(), (list, current) => { list.Add(current.GetType()); return list; }).ToArray();
@@ -33,9 +39,25 @@ namespace IX.Retry.UnitTests
 
             mi = mig.MakeGenericMethod(parameterTypes);
 
-            mi.Invoke(null, )
-            Action x = RetryTestMethod<InvalidOperationException>;
-            x.WithRetry(Policy.TimeBasedRetryPolicy<InvalidOperationException>(TimeSpan.FromSeconds(1)));
+            // Create method parameters
+            IRetryPolicy retryPolicy = Policy.TimeBasedRetryPolicy(TimeSpan.FromSeconds(1), exceptionTypes);
+            CancellationToken cancellationToken = default(CancellationToken);
+
+            List<object> invokeParameters = new List<object>();
+            // TODO: Add delegate parameter here
+            invokeParameters.Add(retryPolicy);
+            invokeParameters.AddRange(parameters);
+            invokeParameters.Add(cancellationToken);
+
+            // ACT
+            // ===
+            var result = mi.Invoke(null, invokeParameters.ToArray());
+
+            // ASSERT
+            // ======
+            Assert.Null(result);
+            //Action x = RetryTestMethod<InvalidOperationException>;
+            //x.WithRetry(Policy.TimeBasedRetryPolicy<InvalidOperationException>(TimeSpan.FromSeconds(1)));
         }
 
         public static IEnumerable<object[]> RetryTheoryDataGenerator()
