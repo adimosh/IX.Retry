79a2f8f36ff43f38a7a3c3d7739df0001421ed03
adimosh <adimosh@live.com>
2015-09-03 18:23:19 +0300

    #12 - Updated references #13 - Added some platform mitigation classes

---
+++ src/IX.Retry/PlatformMitigation/EnumerableExtensions.cs
@@ -0,0 +1,25 @@
+﻿#if MONOANDROID10 || MONOTOUCH10
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace System.Linq
+{
+    public static class EnumerableExtensions
+    {
+        public static bool Any<T>(this IEnumerable<T> source, Func<T, bool> condition)
+        {
+            IEnumerator<T> enumerator = source.GetEnumerator();
+
+            while (enumerator.MoveNext())
+            {
+                if (condition(enumerator.Current))
+                    return true;
+            }
+
+            return false;
+        }
+    }
+}
+#endif
