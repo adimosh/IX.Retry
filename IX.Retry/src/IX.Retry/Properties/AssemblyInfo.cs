4314aabe93a44a5680724ca0ee2c2b5c60e16686
adimosh <adimosh@live.com>
2016-07-21 17:10:58 +0300

    Begun updating IX.Retry for Visual Studio 2015 and .NET Core 1

---
+++ IX.Retry/src/IX.Retry/Properties/AssemblyInfo.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Reflection;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("IX.Retry")]
+[assembly: AssemblyTrademark("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible
+// to COM components.  If you need to access a type in this assembly from
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+#if !NETSTANDARD10
+[assembly: Guid("1bb0f131-f90c-4aa8-8f74-db8f93ffe29a")]
+#endif
